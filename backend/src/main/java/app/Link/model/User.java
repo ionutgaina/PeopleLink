ad16ba0f7d110f7e4514617dfca75c77b9680dcd
Ionut <38911138+ionutgaina@users.noreply.github.com>
2024-04-15 13:15:41 +0300

    added user model (#29)

--- backend/src/main/java/app/Link/model/User.java
+++ backend/src/main/java/app/Link/model/User.java
@@ -1,5 +1,34 @@
 package app.Link.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
 }
