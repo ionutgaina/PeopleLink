a1c8a014b71fd05c6ff622f3672370ab0ea87a14
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-05-02 11:39:58 +0300

    Added repositories. (#39)

---
+++ backend/src/main/java/app/Link/repository/GroupRepository.java
@@ -0,0 +1,7 @@
+package app.Link.repository;
+
+import app.Link.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
---
+++ backend/src/main/java/app/Link/repository/MessageRepository.java
@@ -0,0 +1,7 @@
+package app.Link.repository;
+
+import app.Link.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
