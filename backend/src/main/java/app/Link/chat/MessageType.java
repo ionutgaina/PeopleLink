c563a384b2fb982d97dffc44dc8e44f561e6077c
Ionut Gaina <gainaionutz@gmail.com>
2024-05-20 11:44:48 +0300

    added websockets config

--- backend/src/main/java/app/Link/chat/MessageType.java
+++
@@ -1,7 +0,0 @@
-package app.Link.chat;
-
-public enum MessageType {
-    CHAT,
-    JOIN,
-    LEAVE
-}
--- backend/src/main/java/app/Link/model/User.java
+++ backend/src/main/java/app/Link/model/User.java
@@ -19,10 +19,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username", nullable = false)
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "password", nullable = false)
