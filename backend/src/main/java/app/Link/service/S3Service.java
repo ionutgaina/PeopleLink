54fe493b10e98854621e04f1c4f9c7617ae04797
Ryuuandrei <andreirusei@gmail.com>
2024-05-29 00:21:11 +0300

    s3 config

---
+++ backend/src/main/java/app/Link/service/S3Service.java
@@ -0,0 +1,53 @@
+package app.Link.service;
+
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public void uploadFile(String bucketName, String keyName, String filePath) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(keyName)
+                .build();
+
+        s3Client.putObject(putObjectRequest, Paths.get(filePath));
+    }
+
+    public String upload(MultipartFile file) throws IOException {
+        String fileName = LocalDateTime.now().toString();
+
+        s3Client.putObject(PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(fileName)
+                            .build(),
+                software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes()));
+        return generatePresignedUrl(fileName).toString();
+
+    }
+
+    public URL generatePresignedUrl(String key) {
+        return s3Client.utilities().getUrl(builder -> builder.bucket(bucketName).key(key).build());
+    }
+}
